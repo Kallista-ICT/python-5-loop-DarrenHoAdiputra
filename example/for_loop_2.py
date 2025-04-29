cf7f3d02e6aec2b4753362666c7810b4f1f7a76b
DarrenHoAdiputra <darrenho2008@gmail.com>
2025-04-29 07:22:17 +0000

    Submit

--- example/for_loop_2.py
+++ example/for_loop_2.py
@@ -1,7 +1,15 @@
-#
+# Rolls history (I was realised that this was needed for the code to know the history of the no. of rolls)
+rolls = [4, 2, 6, 3, 5]
+
+# Asks if the player wants to see the history of rolls
+show_history = input("Do you want to see the history of your rolls? (Yes/No): ").strip().lower()
+
+# Display the history if player asks for it
 if show_history == 'yes':
     print("History of your rolls:")
     roll_number = 1
     for roll in rolls:
         print(f"Roll {roll_number}: {roll}")
         roll_number += 1
+else:
+    print("Thank you for playing!")
--- example/while_loop_2.py
+++ example/while_loop_2.py
@@ -6,16 +6,19 @@ rolls = []
 
 # welcome message
 print("Welcome!")
-print("Instructions: ")
+print("Instructions: Roll the dice until your total score reaches 50.\n")
 
 # scoring systems
 while score < 50:
-    input("To roll the dice, enter values from 1-6")
+    input("To roll the dice, enter a value from 1-6")
     die = random.randint(1,6)
-    score +- die
+    score += die
     rolls.append(die)
 
-    print(f"")
-    print(f)
+    print(f"You rolled a {die}")
+    print(f"Current total score: {score}.\n")
 
-# congratulation message, result of totoal score, roll result
+# congratulation message, result of total score, roll result
+print("Congratulations! You have finished the game.")
+print(f"Your total score: {score}")
+print(f"Your dice rolls: {rolls}")
